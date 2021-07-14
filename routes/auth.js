16de8ad2bb4125996851e34280417da3cbb42ebd
david cuspoca <cuspoca3d@gmail.com>
2021-07-14 11:10:01 -0600

    delete service signIn in google

--- routes/auth.js
+++ routes/auth.js
@@ -5,7 +5,7 @@ const { check } = require('express-validator');
 const { validarCampos } = require('../middlewares/validar-campos');
 
 
-const { login, googleSignin } = require('../controllers/auth');
+const { login } = require('../controllers/auth');
 
 
 const router = Router();
@@ -15,12 +15,5 @@ router.post('/login',[
     check('password', 'La contraseña es obligatoria').not().isEmpty(),
     validarCampos
 ],login );
-
-router.post('/google',[
-    check('id_token', 'El id_token es necesario').not().isEmpty(),
-    validarCampos
-], googleSignin );
-
-
 
 module.exports = router;
