7f4ff11875ed090bd1a081e2671820a1750935cc
david cuspoca <cuspoca3d@gmail.com>
2022-01-03 11:59:42 -0600

    implements google singin

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,9 +1,10 @@
-const { response } = require('express');
 const bcryptjs = require('bcryptjs')
 
 const Usuario = require('../models/usuario');
 
 const { generarJWT } = require('../helpers/generar-jwt');
+const { request, response } = require('express');
+const { googleVerify } = require('../helpers/google-verify');
 
 
 const login = async(req, res = response) => {
@@ -52,6 +53,51 @@ const login = async(req, res = response) => {
 
 }
 
+const googleSignIn = async (req= request, res = response) => {
+    const {id_token} = req.body;
+
+    try {
+        const {nombre, img, correo} = await googleVerify(id_token);
+
+        let usuario = await Usuario.findOne({correo});
+        if(!usuario){
+            // creamos usuario
+            const data = {
+                nombre,
+                correo,
+                password: ':P',
+                img,
+                google: true
+            }
+
+            usuario = new Usuario(data);
+            await usuario.save();
+        }
+
+        // si el usuario db
+        if(!usuario.estado){
+            return res.status(401).json({
+                msg: 'hable con el administrador usuario bloqueado'
+            })
+        }
+
+        // genera el jswt
+        const token = await generarJWT(usuario.id)
+
+        res.json({
+            usuario,
+            token
+        })
+    } catch (error) {
+        res.status(400).json({
+            ok: false,
+            msg: 'El token no se pudo verificar'
+        })
+    }
+
+};
+
 module.exports = {
-    login
+    login,
+    googleSignIn
 }
